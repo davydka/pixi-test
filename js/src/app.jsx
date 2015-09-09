23b3054ee3a1b560ba1c840a4fb937ad692adfd1
davidwhitely <david.whitely@codeandtheory.com>
2015-09-09 18:28:26 -0400

    testing pixi in browserify

--- js/src/app.jsx
+++ js/src/app.jsx
@@ -1,14 +1,41 @@
-var React = require('react');
-var Firebase = require('firebase');
+//var React = require('react');
+//var Firebase = require('firebase');
+//
+//var fbRef = new Firebase("https://<firebase-server>.firebaseio.com/");
+//
+//
+//
+//var Hello = React.createClass({
+//	render: function () {
+//		return <h1>Hello</h1>;
+//	}
+//});
+//
+//var element = React.createElement(Hello, {});
+//React.render(element, document.querySelector('.container'));
 
-var fbRef = new Firebase("https://<firebase-server>.firebaseio.com/");
 
 
-var Hello = React.createClass({
-	render: function () {
-		return <h1>Hello</h1>
-	}
-});
 
-var element = React.createElement(Hello, {});
-React.render(element, document.querySelector('.container'));
+var PIXI = require('pixi.js');
+
+var renderer = new PIXI.CanvasRenderer(window.innerWidth, window.innerHeight);
+
+document.body.appendChild(renderer.view);
+
+var stage = new PIXI.Stage;
+
+var zombieTexture = PIXI.Texture.fromImage('/images/zombie.png');
+var zombie = new PIXI.Sprite(zombieTexture);
+
+zombie.position.x = window.innerWidth / 2 - 150;
+zombie.position.y = window.innerHeight / 2 - 150;
+
+stage.addChild(zombie);
+
+function draw() {
+	renderer.render(stage);
+	requestAnimationFrame(draw);
+}
+
+draw();
